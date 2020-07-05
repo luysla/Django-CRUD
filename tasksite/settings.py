d5a9f320d8d840fa8cd82f363ded8f5b7a0c6f5a
luysla <dyana.tavares1@gmail.com>
2020-07-05 19:03:33 -0300

    Adicionei path da pasta static

--- tasksite/settings.py
+++ tasksite/settings.py
@@ -106,5 +106,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/taskapp/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
