1f4d82c31d9bfffbd2285539f6131cdb364d43b6
luysla <dyana.tavares1@gmail.com>
2020-07-06 00:32:49 -0300

    Adicionei formulário para criar tarefa

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('task/add/', views.task_add, name='task_add')
 ]
--- taskapp/views.py
+++ taskapp/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import redirect
+
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
     tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
-    return render(request, 'taskapp/index.html', {'tasks': tasks})
+    return render(request, 'taskapp/task_list.html', {'tasks': tasks})
+
+def task_add(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author = request.user
+            task.created_date = timezone.now()
+            task.save()
+            return redirect('index')
+    else:
+        form = TaskForm()
+    return render(request, 'taskapp/task_add.html', { 'form': form })
